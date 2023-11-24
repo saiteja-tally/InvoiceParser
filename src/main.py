08ebcd8ce640f7ea567255d798c9f8f47d6504de
saiteja-tally <saiteja.g@tallysolutions.com>
2023-11-24 12:20:26 +0530

    updated main.py

--- src/main.py
+++ src/main.py
@@ -1,83 +1,9 @@
-from typing import Optional
+from process_document import process_document_sample
 
-from google.api_core.client_options import ClientOptions
-from google.cloud import documentai  # type: ignore
-
-# TODO(developer): Uncomment these variables before running the sample.
-project_id = "tally-ai-394207"
-location = "us" # Format is "us" or "eu"
-processor_id = "e723cbb45d4557ed" # Create processor before running sample
-file_path = r'D:\DocAI\Tally-Document-AI\data\GST\1.pdf'
-mime_type = "application/pdf" # Refer to https://cloud.google.com/document-ai/docs/file-types for supported file types
-# field_mask = "text,entities,pages.pageNumber"  # Optional. The fields to return in the Document object.
-# processor_version_id = "YOUR_PROCESSOR_VERSION_ID" # Optional. Processor version to use
-
-
-def process_document_sample(
-    project_id: str,
-    location: str,
-    processor_id: str,
-    file_path: str,
-    mime_type: str,
-    field_mask: Optional[str] = None,
-    processor_version_id: Optional[str] = None,
-) -> None:
-    # You must set the `api_endpoint` if you use a location other than "us".
-    opts = ClientOptions(api_endpoint=f"{location}-documentai.googleapis.com")
-
-    client = documentai.DocumentProcessorServiceClient(client_options=opts)
-
-    if processor_version_id:
-        # The full resource name of the processor version, e.g.:
-        # `projects/{project_id}/locations/{location}/processors/{processor_id}/processorVersions/{processor_version_id}`
-        name = client.processor_version_path(
-            project_id, location, processor_id, processor_version_id
-        )
-    else:
-        # The full resource name of the processor, e.g.:
-        # `projects/{project_id}/locations/{location}/processors/{processor_id}`
-        name = client.processor_path(project_id, location, processor_id)
-
-    # Read the file into memory
-    with open(file_path, "rb") as image:
-        image_content = image.read()
-
-    # Load binary data
-    raw_document = documentai.RawDocument(content=image_content, mime_type=mime_type)
-
-    # For more information: https://cloud.google.com/document-ai/docs/reference/rest/v1/ProcessOptions
-    # Optional: Additional configurations for processing.
-    process_options = documentai.ProcessOptions(
-        # Process only specific pages
-        individual_page_selector=documentai.ProcessOptions.IndividualPageSelector(
-            pages=[1]
-        )
-    )
-
-    # Configure the process request
-    request = documentai.ProcessRequest(
-        name=name,
-        raw_document=raw_document,
-        field_mask=field_mask,
-        process_options=process_options,
-    )
-
-    result = client.process_document(request=request)
-
-    # For a full list of `Document` object attributes, reference this page:
-    # https://cloud.google.com/document-ai/docs/reference/rest/v1/Document
-    document = result.document
-
-    # Read the text recognition output from the processor
-    print("The document contains the following text:")
-    print(document.text)
-
-
-
-process_document_sample(
-    project_id,
-    location,
-    processor_id,
-    file_path,
-    mime_type,
+result = process_document_sample(
+    file_path=r'D:\Tally Solutions Pvt. Ltd\Tally-AI - DocAI\Data\GST\1_GST.pdf'
 )
+
+page1_json = result.pages[0]
+
+print(page1_json.form_fields)
